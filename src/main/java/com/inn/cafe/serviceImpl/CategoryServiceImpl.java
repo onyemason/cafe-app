4894a57bfedc8bd4843498c9fc91978b3f8ebdf8
Greate Onyema <greatonyema6@gmail.com>
2025-04-18 18:01:04 +0100

    removed commented out code

--- src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
+++ src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -69,44 +69,5 @@ public class CategoryServiceImpl implements CategoryService {
     }
 }
 
-//
-//    @Override
-//    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
-//        try{
-//            if (!Strings.isBlank(filterValue) && filterValue.equalsIgnoreCase("true")){
-//                log.info("Inside if");
-//                return new ResponseEntity<List<Category>>(categoryDao.getAllCategory(), HttpStatus.OK);
-//            }
-//            return new ResponseEntity<>(categoryDao.findAll(),HttpStatus.OK);
-//
-//        }catch (Exception ex){
-//            ex.printStackTrace();
-//        }
-//        return new ResponseEntity<List<Category>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-//
-//    @Override
-//    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
-//        try{
-//           if(jwtAuthenticationFilter.isAdmin()) {
-//               if(validateCategoryMap(requestMap,true)){
-//                Optional optional = categoryDao.findById(Integer.parseInt(requestMap.get("id")));
-//                if(!optional.isEmpty()){
-//                    categoryDao.save(getCategoryFromMap(requestMap,true));
-//                    return CafeUtils.getResponseEntity("Category Updated Successfully",HttpStatus.OK);
-//
-//                }else{
-//                   return CafeUtils.getResponseEntity("Category id does not exist", HttpStatus.OK);
-//                }
-//               }
-//               return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
-//           }
-//           else {
-//               return CafeUtils.getResponseEntity(CafeConstants.UNAUTHORIZED_ACCESS, HttpStatus.UNAUTHORIZED);
-//           }
-//        }catch (Exception ex){
-//            ex.printStackTrace();
-//        }
-//        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
+
 
